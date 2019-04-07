5a32e361340d66f1b0a6a8d4b13efe8ef16d74a2
ocean1992 <ocean920329@gmail.com>
2019-04-07 15:58:10 +0800

    add a Min method

--- src/raft/util.go
+++ src/raft/util.go
@@ -3,11 +3,18 @@ package raft
 import "log"
 
 // Debugging
-const Debug = 0
+const Debug = 1
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug > 0 {
 		log.Printf(format, a...)
 	}
 	return
+}
+
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
